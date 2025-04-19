b8703f0c8381a5b1f7766694f0f5778e90d5eb5b
Takanobu Furuhashi (古橋 敬信) <59005868+FuruNov@users.noreply.github.com>
2025-04-19 16:14:57 +0900

    Update constants.ts

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -13,8 +13,8 @@ export default {
   feedTitle: '企業テックブログRSS',
   feedDescription: '企業のテックブログの更新をまとめたRSSフィード',
   feedLanguage: 'ja',
-  feedCopyright: 'furunov/rss-feed',
-  feedGenerator: 'furunov/rss-feed',
+  feedCopyright: 'yamadashy/tech-blog-rss-feed',
+  feedGenerator: 'yamadashy/tech-blog-rss-feed',
   feedUrls: {
     atom: `${siteUrl}feeds/atom.xml`,
     rss: `${siteUrl}feeds/rss.xml`,
@@ -22,21 +22,21 @@ export default {
   },
 
   // リンク
-  author: 'furunov',
-  gitHubUserUrl: 'https://github.com/furunov/',
-  gitHubRepositoryUrl: 'https://github.com/furunov/rss-feed/',
-  xUserUrl: 'https://x.com/nov_sin_k',
+  author: 'yamadashy',
+  gitHubUserUrl: 'https://github.com/yamadashy/',
+  gitHubRepositoryUrl: 'https://github.com/yamadashy/tech-blog-rss-feed/',
+  xUserUrl: 'https://x.com/yamadashy',
 
   // Google Analytics系。フォークして使う際は値を空にするか書き換えてください
-  googleSiteVerification: '',
-  globalSiteTagKey: '',
+  googleSiteVerification: 'GPLvXv8kYtLMW912ZS54DKFEZL6ruOrjOFLdHVTo37o',
+  globalSiteTagKey: 'G-CNNNTL0NB3',
 
   // フィードの取得などに使う UserAgent
-  requestUserAgent: 'facebookexternalhit/1.1; furunov/rss-feed',
+  requestUserAgent: 'facebookexternalhit/1.1; yamadashy/tech-blog-rss-feed',
 
   // サイトの追加方法のリンク
   howToAddSiteLink:
-    'https://github.com/furunov/rss-feed#%E3%82%B5%E3%82%A4%E3%83%88%E3%81%AE%E8%BF%BD%E5%8A%A0%E6%96%B9%E6%B3%95',
+    'https://github.com/yamadashy/tech-blog-rss-feed#%E3%82%B5%E3%82%A4%E3%83%88%E3%81%AE%E8%BF%BD%E5%8A%A0%E6%96%B9%E6%B3%95',
 
   // 処理の設定
   feedFetchConcurrency: 50, // フィードを取得する並列数
